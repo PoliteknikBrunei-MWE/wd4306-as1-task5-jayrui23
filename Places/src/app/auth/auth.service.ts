064c89d3aa21d2b2e780477cadd09213d4558ecf
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-03-24 22:25:46 +0800

    added imports and authguard

--- Places/src/app/auth/auth.service.ts
+++ Places/src/app/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
+export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.$userIsAuthenticated;
+  }
+}
 export class AuthService {
   private userIsAuthenticated = false;
 
