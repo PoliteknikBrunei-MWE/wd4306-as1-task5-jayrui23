d049f29163db4fe4068e352aee22661d705d0606
22FTT1519 <22ftt1519@student.pb.edu.bn>
2024-03-24 21:08:40 +0800

    added private places and get

--- Places/src/app/places/places.service.ts
+++ Places/src/app/places/places.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlacesService {
+  private places: Place[] = [
+    new Place(
+      'p1',
+      'Wayne Manor',
+      'In the heart of Gotham City',
+      'https://i.redd.it/icgps7jk00ly.jpg',
+      250.99
+    ),
+    new Place(
+      'p2',
+      'Empire Hotel',
+      'Jerudong area',
+      'https://velvetescape.com/wp-content/uploads/2014/01/P1010897.jpg',
+      400
+    ),
+    new Place(
+      'p3',
+      'Simpsons House',
+      'Somewhere in Springfield',
+      'https://upload.wikimedia.ord/wikipedia/en/c/ca/742_Evergreen_Terrace.png',
+      50
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  get $places() {
+    return [...this.places];
+  }
 }
